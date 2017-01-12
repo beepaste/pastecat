f5de7bdb2119b14281943ebd5172adf19c1fb98f
MohammadAmin Vahedinia <mohammadamin@beeit.ir>
2017-01-12 17:29:45 +0330

    init

---
+++ README.md
@@ -0,0 +1,15 @@
+# pastecat
+Server Side Script For Using `nc` To Paste The Output On A Pastebin
+
+Use `git clone git@github.com:AlirezaieS/pastecat.git && cd pastecat`
+
+Then run `python pastecat.py`
+
+#Usage:
+you need `netcat` Installed To Send The Output To Pastebin.
+
+Pipe Your Command to `nc host 1337` To Send The Output
+
+#Example:
+
+`uname -a | nc localhost 1337`
---
+++ pastecat.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+# -*- Coding: utf-8 -*-
+
+"""
+TODO:
+    1.check if user connected with any data ( if no, then disconnect).
+    2.add timeout
+    3.add multithread [DONE]
+"""
+
+import socket, sys
+from thread import *
+
+HOST = '0.0.0.0'   # Symbolic name meaning all available interfaces
+PORT = 99 # Some Open port on server
+
+def send_req(text="null"):
+    if text != "null":
+        r = requests.post("https://beepaste.ir/api/create", data={"text":"%s" %(text)}, verify=False) # You can use stikked pastebin or my version of it!
+        return r.text.encode("utf-8")[:-1]
+    else:
+        return "Sorry, You Must Enter Something To Paste."
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print 'Socket created'
+
+try:
+    s.bind((HOST, PORT))
+except socket.error as msg:
+    print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+    sys.exit()
+
+print 'Socket bind complete'
+s.listen(10)
+print 'Socket now listening'
+
+#Function for handling connections. This will be used to create threads
+def clientthread(conn, ipaddr):
+    #Receiving from client
+    total_data = []
+    while 1:
+        data = conn.recv(1024)
+        total_data.append(data)
+        if not data:
+            break
+    reply = send_req(''.join(total_data))
+    conn.sendall(reply + '\n')
+    print "Send " + reply + " to " +  ipaddr
+    conn.close()
+
+#now keep talking with the client
+while 1:
+    #wait to accept a connection - blocking call
+    conn, addr = s.accept()
+    print 'Connected with ' + addr[0] + ':' + str(addr[1])
+
+    #start new thread takes 1st argument as a function name to be run, second is the tuple of arguments to the function.
+    start_new_thread(clientthread ,(conn, addr[0]))
+
+s.close()
