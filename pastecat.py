7cd1d83adeba7af649ee8277eb102acd3f336443
MohammadAmin Vahedinia <mohammadamin@beeit.ir>
2017-02-07 20:57:56 +0330

    Added logger!
    Commented print lines and added log function!

--- pastecat.py
+++ pastecat.py
@@ -5,15 +5,26 @@
 TODO:
     1.check if user connected with any data ( if no, then disconnect).
     2.add timeout
-    3.add multithread [DONE]
 """
 
-import socket, sys
+import socket, sys, logging
 from thread import *
 
 HOST = '0.0.0.0'   # Symbolic name meaning all available interfaces
 PORT = 1111 # Some Open port on server
 PASTEBIN = 'https://beepaste.ir/api/create'
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+handler = logging.FileHandler('/var/log/pastecat.log')
+handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+def sendToLog(text="null"):
+    if text != "null":
+        logger.info(text)
 
 def send_req(text="null"):
     if text != "null":
@@ -23,17 +34,21 @@ def send_req(text="null"):
         return "Sorry, You Must Enter Something To Paste."
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print 'Socket created'
+#print 'Socket created'
+sendToLog('Socket Created')
 
 try:
     s.bind((HOST, PORT))
 except socket.error as msg:
-    print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+    #print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+    sendToLog('Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
     sys.exit()
 
-print 'Socket bind complete'
+#print 'Socket bind complete'
+sendToLog('Socket bind complete')
 s.listen(10)
-print 'Socket now listening'
+#print 'Socket now listening'
+sendToLog('Socket now listening')
 
 #Function for handling connections. This will be used to create threads
 def clientthread(conn, ipaddr):
@@ -46,15 +61,16 @@ def clientthread(conn, ipaddr):
             break
     reply = send_req(''.join(total_data))
     conn.sendall(reply + '\n')
-    print "Send " + reply + " to " +  ipaddr
+    #print "Send " + reply + " to " +  ipaddr
+    sendToLog("Send " + reply + " to " +  ipaddr)
     conn.close()
 
 #now keep talking with the client
 while 1:
     #wait to accept a connection - blocking call
     conn, addr = s.accept()
-    print 'Connected with ' + addr[0] + ':' + str(addr[1])
-
+    #print 'Connected with ' + addr[0] + ':' + str(addr[1])
+    sendToLog('Connected with ' + addr[0] + ':' + str(addr[1]))
     #start new thread takes 1st argument as a function name to be run, second is the tuple of arguments to the function.
     start_new_thread(clientthread ,(conn, addr[0]))
 
