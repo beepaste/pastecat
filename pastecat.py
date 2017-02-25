52574fcb4fbc3a75bc3c99a3c4759f8e3164863e
Alireza Mosajjal <mosajjal@gmail.com>
2017-02-25 10:48:00 +0330

    fixes for PEP compatibility and best practices

--- pastecat.py
+++ pastecat.py
@@ -1,5 +1,4 @@
-#!/usr/bin/python
-# -*- Coding: utf-8 -*-
+#!/usr/bin/env python
 
 """
 TODO:
@@ -7,11 +6,16 @@ TODO:
     2.add timeout
 """
 
-import socket, sys, logging, requests
-from thread import *
+import logging
+import socket
+import sys
+
+import requests
+
+from _thread import start_new_thread
 
 HOST = '0.0.0.0'   # Symbolic name meaning all available interfaces
-PORT = 1111 # Some Open port on server
+PORT = 1111  # Some Open port on server
 PASTEBIN = 'https://beepaste.ir/api/create'
 
 logger = logging.getLogger(__name__)
@@ -22,37 +26,42 @@ formatter = logging.Formatter('%(asctime)s - %(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
+
 def sendToLog(text="null"):
     if text != "null":
         logger.info(text)
 
+
 def send_req(text="null"):
     if text != "null":
-        r = requests.post(PASTEBIN, data={"text":"%s" %(text)}, verify=False) # You can use stikked pastebin or my version of it!
+        r = requests.post(PASTEBIN, data={"text": "%s" % (text)}, verify=False)  # You can use stikked pastebin or my version of it!
         return r.text.encode("utf-8")[:-1]
     else:
         return "Sorry, You Must Enter Something To Paste."
 
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#print 'Socket created'
+# print('Socket created')
 sendToLog('Socket Created')
 
 try:
     s.bind((HOST, PORT))
-except socket.error as msg:
-    #print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
-    sendToLog('Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
+except OSError as err:
+    err_no, err_msg = err.args
+    # print('Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
+    sendToLog('Bind failed. Error Code : ' + str(err_no) + ' Message ' + err_msg)
     sys.exit()
 
-#print 'Socket bind complete'
+# print('Socket bind complete')
 sendToLog('Socket bind complete')
 s.listen(10)
-#print 'Socket now listening'
+# print('Socket now listening')
 sendToLog('Socket now listening')
 
-#Function for handling connections. This will be used to create threads
+
+# Function for handling connections. This will be used to create threads
 def clientthread(conn, ipaddr):
-    #Receiving from client
+    # Receiving from client
     total_data = []
     while 1:
         data = conn.recv(1024)
@@ -61,17 +70,18 @@ def clientthread(conn, ipaddr):
             break
     reply = send_req(''.join(total_data))
     conn.sendall(reply + '\n')
-    #print "Send " + reply + " to " +  ipaddr
-    sendToLog("Send " + reply + " to " +  ipaddr)
+    # print ("Send " + reply + " to " +  ipaddr)
+    sendToLog("Send " + reply + " to " + ipaddr)
     conn.close()
 
-#now keep talking with the client
-while 1:
-    #wait to accept a connection - blocking call
+
+# now keep talking with the client
+while True:
+    # wait to accept a connection - blocking call
     conn, addr = s.accept()
-    #print 'Connected with ' + addr[0] + ':' + str(addr[1])
+    # print('Connected with ' + addr[0] + ':' + str(addr[1]))
     sendToLog('Connected with ' + addr[0] + ':' + str(addr[1]))
-    #start new thread takes 1st argument as a function name to be run, second is the tuple of arguments to the function.
-    start_new_thread(clientthread ,(conn, addr[0]))
+    # start new thread takes 1st argument as a function name to be run, second is the tuple of arguments to the function.
+    start_new_thread(clientthread, (conn, addr[0]))
 
 s.close()
